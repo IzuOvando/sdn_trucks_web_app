b0c52a11f5f50218da79d1fea0111fa0f985528a
IzuOvando <voja210394@gmail.com>
2025-04-10 19:01:50 -0600

    solves

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     env: {
       BASE_URL: process.env.BASE_URL || "http://localhost:3000",
-      ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-      REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
     },
   };
   
--- src/config/constants.tsx
+++ src/config/constants.tsx
@@ -3,10 +3,6 @@ const CONSTANTS = {
       DEFAULT_PAGE: 1,
       DEFAULT_LIMIT: 10,
     },
-    TOKEN_EXPIRACY: {
-        ACCESS: process.env.TOKEN_EXPIRACY_ACCESS || '1h',
-        REFRESH: process.env.TOKEN_EXPIRACY_REFRESH || '1d',
-      },
   };
   
   export default CONSTANTS;
