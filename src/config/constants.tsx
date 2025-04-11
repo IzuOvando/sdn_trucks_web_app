dbb9e6a7a125413019ef11b1b363474797a5f5a3
IzuOvando <70975398+IzuOvando@users.noreply.github.com>
2025-04-10 19:21:25 -0600

    config (#1)
    * config
    
    * comments
    
    * solves
    
    * solves

---
+++ src/config/constants.tsx
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+    PAGINATION: {
+      DEFAULT_PAGE: 1,
+      DEFAULT_LIMIT: 10,
+    },
+  };
+  
+  export default CONSTANTS;
---
+++ src/config/variables.tsx
@@ -0,0 +1,5 @@
+const VARIABLES = {
+    BASE_URL: process.env.BASE_URL || "http://localhost:3000",
+};
+  
+export default VARIABLES;
